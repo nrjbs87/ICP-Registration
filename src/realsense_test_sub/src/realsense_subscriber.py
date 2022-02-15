af3ec9d2a6d39b7ae2401716fe0df2e9e5be18e8
Neeraj Basu <nrjbs87@gmail.com>
2022-02-14 21:08:19 -0500

    got rgb sub working, depth next

--- src/realsense_test_sub/src/realsense_subscriber.py
+++ src/realsense_test_sub/src/realsense_subscriber.py
@@ -2,35 +2,50 @@
 
 import cv_bridge 
 import rospy
+import cv2
+import numpy as np
 from sensor_msgs.msg import Image
+
+'''
+https://answers.ros.org/question/354676/how-can-i-bridge-and-display-images-from-realsense-d435-camera/
+http://wiki.ros.org/cv_bridge/Tutorials/ConvertingBetweenROSImagesAndOpenCVImagesPython
+http://wiki.ros.org/rqt_image_view
+'''
 
 class Depth_Retriever():
     def __init__(self):
+    
+        self.image_pub = rospy.Publisher("depth_topic", Image)
 
-        self.bridge = cv_bridge.CvBridge()
+        # self.bridge = cv_bridge.CvBridge()
 
-        self.sub = rospy.Subscriber('/camera/depth/image_raw', Image, self.callback)
+        #self.sub = rospy.Subscriber('/camera/depth/image_raw', Image, self.callback)
+        self.sub = rospy.Subscriber('/camera/color/image_raw', Image, self.callback)
 
     def callback(self,ros_pics):
-        try: 
-            my_image = self.bridge.imgmsg_to_cv2(ros_pics, encoding = "passthrough")
 
-        except cv_bridge.CvBridgeError as e:
-            print("CvBridge could not convert images from realsense to opencv")
-        height,width, channels = my_image.shape
-        my_height = my_image.shape[0]
-        print(my_height)
+        my_image = np.frombuffer(ros_pics.data, dtype=np.uint8).reshape(ros_pics.height, ros_pics.width, -1)
 
-        self.vert_len = ros_pics.height # retrieve height information from Image msg
-        print(self.vert_len)
-        rospy.logwarn("HERE")
+        # height,width, channels = my_image.shape
+        
+        cv2.imshow("Image window", my_image)
+        cv2.waitKey(3)
 
+        
+        my_image = np.frombuffer(ros_pics.data, dtype=np.uint8).reshape(ros_pics.height, ros_pics.width, -1)
 
+        self.image_pub.publish(my_image)
+   
 def main():
+
+    dr = Depth_Retriever()
     rospy.init_node("Depth",anonymous = True)
 
-    rospy.spin()
-
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down")
+    cv2.destroyAllWindows()
 
 if __name__ == '__main__':
 
