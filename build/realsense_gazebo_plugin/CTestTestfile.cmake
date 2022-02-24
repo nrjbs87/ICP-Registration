dbb6d20f80eb4d61c27795b884b48669f89892a1
Neeraj Basu <nrjbs87@gmail.com>
2022-02-24 16:25:53 -0500

    deleting unused files

--- build/realsense_gazebo_plugin/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/neerajb/Desktop/voiage_ws/src/realsense_gazebo_plugin
-# Build directory: /home/neerajb/Desktop/voiage_ws/build/realsense_gazebo_plugin
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/realsense_test_sub/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/neerajb/Desktop/voiage_ws/src/realsense_test_sub
-# Build directory: /home/neerajb/Desktop/voiage_ws/build/realsense_test_sub
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
